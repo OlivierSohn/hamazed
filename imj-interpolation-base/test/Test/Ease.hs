578f16c13754938d7be15333ed47a34af47ca889
Olivier Sohn <olivier.sohn@gmail.com>
2017-12-21 01:51:22 +0100

    tentative to fix ghc8.2 warnings

--- imj-interpolation-base/test/Test/Ease.hs
+++ imj-interpolation-base/test/Test/Ease.hs
@@ -12,10 +12,10 @@ testEase = do
   test quartInOut
 
 test :: (Float -> Float) -> IO ()
-test f = mapM_ (\v -> putStrLn $ show v) $ map f $ map (\i -> fromIntegral i / 10.0) [0..10]
+test ease = mapM_ (\v -> putStrLn $ show v) $ map ease $ map (\i -> fromIntegral i / 10.0) [(0 :: Int)..10]
 
 quartInOut :: Float -> Float
 quartInOut time =
     if time < 0.5
-    then        1 / 2 *  2^4 * time  * time  * time  * time
-    else negate 1 / 2 * (2^4 * (time-1) * (time-1) * (time-1) * (time-1) - 2)
+    then        1 / 2 *  2**4 * time  * time  * time  * time
+    else negate 1 / 2 * (2**4 * (time-1) * (time-1) * (time-1) * (time-1) - 2)

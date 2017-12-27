90248645dad30bf13144bb43f155493205dd62d8
Olivier Sohn <olivier.sohn@gmail.com>
2017-12-27 07:15:43 +0100

    move test

--- imj-base/test/Spec.hs
+++ imj-base/test/Spec.hs
@@ -1,9 +1,16 @@
---import Test.Imj.Stdout
---import Test.Imj.Ease
-import Test.Imj.Vector
-import Test.Imj.Bresenham3
-import Test.Imj.Timing
-import Test.Imj.Interpolation
+import           System.Console.ANSI(clearScreen)
+import           Control.Monad.Reader(runReaderT)
+
+import           Imj.Draw
+import           Imj.Render.Naive
+
+--import           Test.Imj.Stdout
+--import           Test.Imj.Ease
+import           Test.Imj.Vector
+import           Test.Imj.Bresenham3
+import           Test.Imj.Timing
+import           Test.Imj.Interpolation
+import           Test.Imj.InterpolatedColorString
 
 main :: IO ()
 main = do
@@ -12,6 +19,11 @@ main = do
   testTiming >>= print
   testVector >>= print
   testInterpolation
+
+  clearScreen -- to not overwrite current terminal content.
+  runReaderT (testICS >>
+              renderDrawing
+              ) (NaiveDraw 2)
 
   --testStdout
   --testEase
--- imj-engine/test/Spec.hs
+++ imj-engine/test/Spec.hs
@@ -1,17 +1,4 @@
-import           System.Console.ANSI(clearScreen)
-
-import           Control.Monad.Reader(runReaderT)
-
-import           Imj.Draw
-import           Imj.Render.Naive
-
-import           Test.Imj.InterpolatedColorString
 
 main :: IO ()
 main = do
   putStrLn "" -- for readablilty
-
-  clearScreen -- to not overwrite current terminal content.
-  runReaderT (testICS >>
-              renderDrawing
-              ) (NaiveDraw 2)

2eb8679a6d0a4a78a343f9317e3a3a840b9b2533
Olivier Sohn <olivier.sohn@gmail.com>
2018-02-19 22:58:09 +0100

    Fix console input

--- imj-base/src/Imj/Input/Blocking.hs
+++ imj-base/src/Imj/Input/Blocking.hs
@@ -15,7 +15,7 @@ import           Imj.Prelude
 import           System.IO( getChar, hReady, stdin )
 import           System.Timeout( timeout )
 
-import           Data.Char( ord )
+import           Data.Char( ord, isControl )
 import           Data.List( reverse )
 
 import           Imj.Geo.Discrete.Types( Direction(..) )
@@ -33,43 +33,41 @@ getKeyThenFlush =
 -- This way, we won't interrupt reads of escaped characters.
 timeoutGetKeyThenFlush :: Int -> IO (Maybe Key)
 timeoutGetKeyThenFlush dt =
-  fmap fromString <$> timeoutGetAllChars dt
+  timeoutGetAllChars dt >>= \mayStr -> do
+    --putStrLn $ show mayStr
+    return $ fmap fromString mayStr
 
 fromString :: String -> Key
-fromString =
-  \case
-    [] -> error "should not be empty"
-    [c] -> case ord c of -- according to http://www.asciitable.com/
-      8   -> BackSpace
-      9   -> Tab
-      10  -> Enter -- TODO 13 is carriage return, should we handle it?
-      27  -> Escape
-      127 -> BackSpace
-      o ->
-        if o >= 32 && o <= 126
+fromString = \case
+  [] -> error "should not be empty"
+  [c] -> case ord c of -- according to http://www.asciitable.com/
+    8   -> BackSpace
+    9   -> Tab
+    10  -> Enter -- TODO 13 is carriage return, should we handle it?
+    27  -> Escape
+    127 -> BackSpace
+    _ -> if isControl c
           then
-            AlphaNum c
-          else
             Unknown
-    c:l -> case ord c of
-      27 {-ESC-} -> case l of
-        a:b:_ -> case a of
-          '[' -> case b of
-            'A' -> Arrow Up
-            'B' -> Arrow Down
-            'C' -> Arrow RIGHT
-            'D' -> Arrow LEFT
-            _   -> Unknown
-          _ -> Unknown
+          else
+            AlphaNum c
+  c:l -> case ord c of
+    27 {-ESC-} -> case l of
+      a:b:_ -> case a of
+        '[' -> case b of
+          'A' -> Arrow Up
+          'B' -> Arrow Down
+          'C' -> Arrow RIGHT
+          'D' -> Arrow LEFT
+          _   -> Unknown
         _ -> Unknown
-      _ -> AlphaNum c -- TODO we ignore the rest, should we use it in case of complex characters?
+      _ -> Unknown
+    _ -> AlphaNum c -- TODO we ignore the rest, should we use it in case of complex characters?
 
 -- | returns when stdin is empty, or when the timeout was hit for the first character read.
 timeoutGetAllChars :: Int -> IO (Maybe String)
-timeoutGetAllChars dt = do
-  res <- fmap reverse <$> getKey' ""
-  --putStrLn $ show res
-  return res
+timeoutGetAllChars dt =
+  fmap reverse <$> getKey' ""
  where
   getKey' chars =
     -- timeout only applies to the first character read:

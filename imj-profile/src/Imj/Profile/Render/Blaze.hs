599c9694f1381f11efa63ba879819edd376146f8
Olivier Sohn <olivier.sohn@gmail.com>
2018-04-06 03:10:32 +0200

    Fix usage of Utf8 renderer

--- imj-profile/src/Imj/Profile/Render/Blaze.hs
+++ imj-profile/src/Imj/Profile/Render/Blaze.hs
@@ -13,10 +13,10 @@ module Imj.Profile.Render.Blaze
 
 import           Imj.Prelude
 import           Prelude(FilePath, writeFile)
-import qualified Data.ByteString as BS(writeFile)
+import qualified Data.ByteString.Lazy as BS(writeFile)
 
 import           Text.Blaze.Html5 (Html, ToMarkup(..), docTypeHtml, body, toHtml)
-import           Text.Blaze.Html.Renderer.Utf8(renderHtmlToByteStringIO)
+import qualified Text.Blaze.Html.Renderer.Utf8 as Utf8(renderHtml)
 import qualified Text.Blaze.Html.Renderer.String as Str(renderHtml)
 import qualified Text.Blaze.Html.Renderer.Pretty as Pretty(renderHtml)
 import qualified Text.Blaze.Html5 as H
@@ -40,6 +40,6 @@ resultLine e = do
 
 renderHtml :: FilePath -> Html -> IO ()
 renderHtml path html = do
-  renderHtmlToByteStringIO (BS.writeFile $ "z.utf8." <> path) html
+  BS.writeFile ("z.utf8." <> path) $ Utf8.renderHtml html
   writeFile ("z.string." <> path) $ Str.renderHtml html
   writeFile ("z.pretty." <> path) $ Pretty.renderHtml html

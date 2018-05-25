32d3fbd96f2cdae5dac722b5d9769ba201ff2bfe
Olivier Sohn <olivier.sohn@gmail.com>
2018-05-25 19:26:36 +0200

    Fix compilation

--- imj-game-hamazed/src/Imj/Game/Hamazed/Music.hs
+++ imj-game-hamazed/src/Imj/Game/Hamazed/Music.hs
@@ -24,8 +24,7 @@ scoreForLevel (LevelSpec n _) =
   idx = (fromIntegral (n-firstLevel)) `mod` nScores
 
 hamazedScores :: [Score]
-hamazedScores = [intersperse Rest quatScore]
-{-
+hamazedScores =
   [ transpose 2 primaryScore
   , transpose 2 secondaryScore
   , transpose 2 tertiaryScore
@@ -33,7 +32,7 @@ hamazedScores = [intersperse Rest quatScore]
   , transpose (-2) quintScore
   , sextScore
   ]
--}
+
 primaryScore :: Score
 primaryScore = mkScore [firstVoice, secondVoice, thirdVoice]
  where

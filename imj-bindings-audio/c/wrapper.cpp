abaefcf75901deddaaad04de8348d9a18a00924d
Olivier Sohn <olivier.sohn@gmail.com>
2018-06-10 02:44:54 +0200

    Pragma for clang / gcc versions

--- imj-bindings-audio/c/wrapper.cpp
+++ imj-bindings-audio/c/wrapper.cpp
@@ -1,3 +1,16 @@
+
+#ifdef __clang_major__
+#  pragma message ("compiled with clang " __clang_major__ "." __clang_minor__"."__clang_patchlevel__);
+#endif
+
+#ifdef __GNUC__
+    // note that clang 3.7 declares itself as a gcc 4.2"
+#  pragma message ("compiled with gcc " __GNUC__ "." __GNUC_MINOR__"."__GNUC_PATCHLEVEL__);
+#endif
+
+// if version of visualc++ is needed, do the same as here:
+// https://github.com/boostorg/predef/blob/develop/include/boost/predef/compiler/visualc.h
+
 #ifdef __cplusplus
 
 #include "cpp.audio/include/public.h"

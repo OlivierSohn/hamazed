59bcbab8e558ea7411e8b37cac9614bc3acb5784
Olivier Sohn <olivier.sohn@gmail.com>
2018-07-05 12:28:26 +0200

    Use weeder ignore feature

--- imj-audio/srcDummy/Imj/Audio/Dummy.hs
+++ imj-audio/srcDummy/Imj/Audio/Dummy.hs
@@ -4,7 +4,7 @@
 -- | Dummy module to fix https://github.com/haskell/cabal/issues/944
 -- but then, we have this other issue : https://github.com/haskell/cabal/issues/4215
 --
--- The doc can only be generated manually, once the dependency from teh main library
+-- The doc can only be generated manually, once the dependency from the main library
 -- to the convenience library has been removed.
 
 module Imj.Audio.Dummy
